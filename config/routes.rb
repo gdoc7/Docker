af53a99659a1631e9fc6f08a7969db0245308bc4
gdoc7 <gabrielojeda7@gmail.com>
2018-12-24 01:51:02 -0400

    Inicio de sesion Web service

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  #devise_for :users
+	namespace :api, defaults:{ format: :json }do 
+	  resources :users, :only => [:show, :create, :update, :destroy]
+	end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
